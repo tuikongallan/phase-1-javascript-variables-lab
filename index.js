22bf7e11b2d93bb7803575acaff8202e2ec8de07
Allan <allan.tuikong@student.moringaschool.com>
2022-08-17 10:46:25 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+//let companyName = 'Scuber';
+const companyName = 'Scuber';
+//const companyCeo = 'Susan Smith';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+
